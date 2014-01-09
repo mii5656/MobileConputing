c247d45d33eb3278c6cc923115f91881e5365f4e
shun443 <shun@ubi.cs.ritsumei.ac.jp>
2014-01-09 19:53:40 +0900

    add result activity

---
+++ ScrambledEgg/src/jp/ac/ritsumei/scrambledegg/room/ResultActivity.java
@@ -0,0 +1,41 @@
+package jp.ac.ritsumei.scrambledegg.room;
+
+import jp.ac.ritsumei.scrambledegg.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class ResultActivity extends Activity{
+	TextView resultText1;
+	TextView resultText2;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// タイトルを非表示にします。
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// splash.xmlをViewに指定します。
+		setContentView(R.layout.result);
+		
+		Intent intent = getIntent();
+		
+		int myFinalEggs = intent.getIntExtra("myFinalEggs", 0);
+		int enemyFinalEggs = intent.getIntExtra("enemyFinalEggs", 0);
+		
+		resultText1 = (TextView) findViewById(R.id.resultText1);
+
+		resultText2 = (TextView) findViewById(R.id.resultText2);
+		
+		resultText1.setText(myFinalEggs + " - " + enemyFinalEggs);
+		
+		if (myFinalEggs > enemyFinalEggs) {
+			resultText2.setText("WIN!: Your team cooked bigger Scrambled Egg!!");
+		} else if(myFinalEggs < enemyFinalEggs) {
+			resultText2.setText("LOSE: Your team cooked smaller Scrambled Egg...");
+		} else {
+			resultText2.setText("DRAW");
+		}
+		
+	}
+}
